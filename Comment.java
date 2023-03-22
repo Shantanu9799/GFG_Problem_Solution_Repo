f385b2368677e7e006ac4d1e6a7c2776318dd061
Shantanu Chatterjee <cshantanu712@gmail.com>
2023-03-22 11:41:05 +0530

    adding 7 problems

---
+++ Comment.java
@@ -0,0 +1,21 @@
+//User function Template for Java
+
+
+//Back-end complete function Template for Java
+class Geeks {
+    static void isDivisibleByPrime (int n) 
+    {
+            //Your code here
+            if(n%11 == 0){
+                System.out.println("Eleven");
+            } else if(n%3 == 0){
+                System.out.println("Three");
+            } else if(n%2 == 0){
+                System.out.println("Two");
+            } else{
+                System.out.println("-1");
+            }
+           
+    }
+
+}
