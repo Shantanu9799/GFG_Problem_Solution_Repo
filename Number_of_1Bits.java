34df8a0de9507b2a9803710763e758f6e9559c9b
Shantanu Chatterjee <cshantanu712@gmail.com>
2023-05-11 16:20:40 +0530

    no of 1 bits

---
+++ Number_of_1Bits.java
@@ -0,0 +1,13 @@
+class Solution {
+    static int setBits(int n) {
+        // code here
+        int cnt = 0;
+        while(n > 0) {
+            if((n&1) == 1) {
+                cnt++;
+            }
+            n = n >> 1;
+        }
+        return cnt;
+    }
+}
