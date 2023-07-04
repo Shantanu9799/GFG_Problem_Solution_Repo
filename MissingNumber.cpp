62167931c77a241ee90c2d44cdb235f1a95b2933
Shantanu9799 <cshantanu712@gmail.com>
2023-07-04 21:22:45 +0530

    dsa cheet sheet

---
+++ MissingNumber.cpp
@@ -0,0 +1,11 @@
+class Solution{
+  public:
+    int missingNumber(vector<int>& array, int n) {
+        // Your code goes here
+        int totalSum = (n * (n+1)) / 2;
+        for(auto it : array) {
+            totalSum -= it;
+        }
+        return totalSum;
+    }
+};
